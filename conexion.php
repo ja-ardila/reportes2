4e07a6cc377d0d22d0727424b971db6f1eafc440
Carlos Tiguaque <c.tiguaque@uniandes.edu.co>
2025-07-21 21:04:15 -0500

    Se ajusta apuntamiento a nueva bdd

--- conexion.php
+++ conexion.php
@@ -5,7 +5,7 @@ $contrasena = "Zsw2Xaq1";
 $base_de_datos = "jardila_reportes2"; 
 
 // Crear conexión
-$conexion = new mysqli("localhost:3306", "jardila_reportes", "Zsw2Xaq1", "jardila_reportes2");
+$conexion = new mysqli("jardila-reportes2.cuhps5uu7rzq.us-east-1.rds.amazonaws.com:3306", "jardila_reportes", "Zsw2Xaq1", "jardila_reportes2");
 
 // Verificar conexión
 if ($conexion->connect_error) {
